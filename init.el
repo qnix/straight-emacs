b949954a803acbd86fe37c0cb351506ea41ee4ef
Quan Ta <quancta@gmail.com>
2021-03-08 17:04:16 -0800

    Update initial package configurations

--- init.el
+++ init.el
@@ -24,12 +24,54 @@
 (setq inhibit-startup-message t
       initial-scratch-message nil)
 
+(setq tab-width 2
+      indent-tabs-mode nil)
+
+(straight-use-package 'use-package)
 (straight-use-package 'better-defaults)
 
 (straight-use-package 'evil)
 (straight-use-package 'evil-collection)
+(straight-use-package 'general)
 
-(setq evil-want-integration t)
-(setq evil-want-keybinding nil)
-(evil-mode 1)
-(evil-collection-init)
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package general
+  :straight t
+  :after evil
+  :config
+  (general-create-definer qnix/leader-keys
+			  :keymaps '(normal insert visual emacs)
+			  :prefix "SPC"
+			  :global-prefix "C-SPC")
+
+  (qnix/leader-keys
+   "fe" '(lambda () (interactive) (find-file (expand-file-name "init.el" user-emacs-directory)))))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+
+(use-package magit
+  :straight t
+  :commands (magit-status)
+  :bind (("C-x C-g" . magit-status))
+  :config
+  (defadvice magit-status (around magit-fullscreen activate)
+    "Make magit-status run alone in a frame."
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-window))
+
+  (defun magit-quit-session ()
+    "Restore the previous window configuration and kill the magit buffer."
+    (interactive)
+    (kill-buffer)
+    (jump-to-register :magit-fullscreen)))
